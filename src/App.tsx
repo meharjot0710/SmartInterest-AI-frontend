2f0faf3bbdb5bf1bc8148843c72eefc3f2ae4e48
TheYuvrajMishra <yuvraj17mishra11@gmail.com>
2025-03-28 23:51:51 +0530

    changes in directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 "use client";
 import React from "react";
-import Header from "./components/NavBar.tsx";
-import HeroSection from "./components/HeroSection.tsx";
-import CareerPathsSection from "./components/CareerPathsSection.tsx";
-import TrustSection from "./components/TrustSection";
-import FAQSection from "./components/FaqSection";
-import Footer from "./components/Footer.tsx";
+import Landing from "./pages/Landing.tsx";
 import SignUpLogIn from "./components/SignUpLogIn.tsx";
 
 const SmartInterestAI: React.FC = () => {
   return (
     <main className="min-h-screen bg-gradient-to-tr from-black to-[#1a0e2b] overflow-x-hidden">
-      <Header />
-      <HeroSection />
-      <CareerPathsSection />
-      <TrustSection />
-      <FAQSection />
-      <Footer />
-      <SignUpLogIn/>
+      <Landing/>
+      
     </main>
   );
 };
